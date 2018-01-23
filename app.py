df7fff4320a1fea5c21b32daf81ba64c404d3c76
Amin <aminrahman20@gmail.com>
2018-01-23 15:22:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 #test
-time_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
 #print(air_date)
 client = MongoClient('mongodb://root:root@ds211558.mlab.com:11558/air_data')
 db=client.get_database('air_data')
@@ -16,13 +16,14 @@ def hello_world():
    return 'Hello World'
 
 @app.route('/val/<co>/<dust>',methods = ['GET'])
-def val(co=None,dust=None,time_date=None):
-	print(co+" "+dust+" "+time_date)	
-	db.air_datas.insert_one({'co': co,'dust':dust,'time_date':time_date})
-	return co+" "+dust+" "+time_date
+   def val(co=None,dust=None,time_date=None):
+   time_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+   print(co+" "+dust+" "+time_date)	
+   db.air_datas.insert_one({'co': co,'dust':dust,'time_date':time_date})
+   return co+" "+dust+" "+time_date
 @app.route('/<area>',methods = ['GET'])
 def val(area=None):
-	print(area)	
+   print(area)	
 	#db.air_datas.insert_one({'co': co,'dust':dust})
 	
 	return "{'co':'211','du':'211'}"
