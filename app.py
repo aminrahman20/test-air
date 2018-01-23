cf4fba40bdaa32036666bde17f4890810bcd05d6
Amin <aminrahman20@gmail.com>
2018-01-23 15:12:32 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import os
 from flask import Flask
 from flask import request
 from pymongo import MongoClient
-app = Flask(__name__)
-#test
+from datetime import datetime
 
+app = Flask(__name__)
+
+#test
+time_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#print(air_date)
 client = MongoClient('mongodb://root:root@ds211558.mlab.com:11558/air_data')
 db=client.get_database('air_data')
 @app.route('/')
@@ -12,10 +16,10 @@ def hello_world():
    return 'Hello World'
 
 @app.route('/val/<co>/<dust>',methods = ['GET'])
-def val(co=None,dust=None):
-	print(co+" "+dust)	
-	db.air_datas.insert_one({'co': co,'dust':dust})
-	return co+" "+dust
+def val(co=None,dust=None,time_date=None):
+	print(co+" "+dust+" "+time_date)	
+	db.air_datas.insert_one({'co': co,'dust':dust,'time_date':time_date})
+	return co+" "+dust+" "+time_date
 @app.route('/<area>',methods = ['GET'])
 def val(area=None):
 	print(area)	
