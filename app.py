1a86383e252fdab53423c0c42dbe7add1fd49093
Amin <aminrahman20@gmail.com>
2018-01-29 23:15:29 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 from flask import Flask
 from flask import request
@@ -14,29 +13,29 @@ client = MongoClient('mongodb://root:root@ds211558.mlab.com:11558/air_data')
 db=client.get_database('air_data')
 @app.route('/')
 def hello_world():
-   return 'Hello World'
+    return 'Hello World'
 @app.route('/time',methods = ['GET'])
 def time():
    
-   #x=datetime.datetime.now()
-   #y=datetime.datetime.now() - datetime.timedelta(minutes=20)
-   #db.air_datas.find({time_date: {$gte:y,$lt: x}})
-   results = db.air_datas.find()
-      for record in results:
-         print(record['name'])
+    #x=datetime.datetime.now()
+    #y=datetime.datetime.now() - datetime.timedelta(minutes=20)
+    #db.air_datas.find({time_date: {$gte:y,$lt: x}})
+    results = db.air_datas.find_one()
+    for record in results:
+        print(record['co'])
  
    
-   #return "amin"
+    #return "amin"
    
 @app.route('/val/<co>/<dust>',methods = ['GET'])
 def val(co=None,dust=None):
    
-   time_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-   print(time_date+" "+co+" "+dust)	
-   db.air_datas.insert_one({'time_date':time_date,'co': co,'dust':dust})
+    time_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(time_date+" "+co+" "+dust)	
+    db.air_datas.insert_one({'time_date':time_date,'co': co,'dust':dust})
   
-   return time_date+" "+co+" "+dust
+    return time_date+" "+co+" "+dust
    
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
