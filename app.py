48f8b81ab62f0141171aa8f0276ce3c40c4e96a2
Amin <aminrahman20@gmail.com>
2018-01-29 22:46:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def time():
    #x=datetime.datetime.now()
    #y=datetime.datetime.now() - datetime.timedelta(minutes=20)
    #db.air_datas.find({time_date: {$gte:y,$lt: x}})
-   print(db.air_datas.find_one())
+   print(db.air_datas.find())
    #return "amin"
    
 @app.route('/val/<co>/<dust>',methods = ['GET'])
