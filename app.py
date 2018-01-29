3c295a7adb6b52b8eb54eba25eb7fe74f2435df9
Amin <aminrahman20@gmail.com>
2018-01-29 21:04:07 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-from flask.ext.pymongo import pymongo
+from flask.ext.pymongo import PyMongo
 from flask import request
 #from pymongo import MongoClient
 from datetime import datetime
