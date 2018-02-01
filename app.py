63e04303708c42d0d21f3e48b51463e7329464eb
Amin <aminrahman20@gmail.com>
2018-02-01 16:16:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ def time():
     #return time_date+" "+co+" "+dust
 @app.route('/val/<co>',methods = ['GET'])
 def val(co=None):
-    time_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=6)
+    time_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    time_date = time_date+ datetime.timedelta(hours=6)
     print(time_date+" "+co)	
     db.air_datas.insert_one({'time_date':time_date,'co': co})
     return time_date+" "+co
