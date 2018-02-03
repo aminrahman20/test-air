88526c919b7ca7114afa6dd3bcdcbdc9548e070d
Amin <aminrahman20@gmail.com>
2018-02-03 23:15:48 +0600

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def hello_world():
 @app.route('/time',methods = ['GET'])
 def time():
     results = db.air_datas.find().sort("time_date",-1)
-    return results[0]['co']
+    #return results[0]['co']
     #x=datetime.datetime.now()
     #y=datetime.datetime.now() - datetime.timedelta(minutes=20)
     #db.air_datas.find({time_date: {$gte:y,$lt: x}})
@@ -30,6 +30,7 @@ def time():
         #print(record['co'])
     #results = str(results)
     #return results
+    return jsonify(results)
     #json_data = json.loads(results)
     
     #return json_data
