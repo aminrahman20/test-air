3101416eddfdaf59f0ed4c3130ed8182a4495478
Amin <aminrahman20@gmail.com>
2018-02-03 23:09:25 +0600

    Update app.py

--- app.py
+++ app.py
@@ -23,13 +23,14 @@ def time():
     #x=datetime.datetime.now()
     #y=datetime.datetime.now() - datetime.timedelta(minutes=20)
     #db.air_datas.find({time_date: {$gte:y,$lt: x}})
-    results = request.json(db.air_datas.find().sort("time_date",-1))
+    #results = request.json(db.air_datas.find().sort("time_date",-1))
+    results = db.air_datas.find().sort("time_date",-1)
     #for record in results:
         #print(record['co'])
-    results = str(results)
-    return results
+    #results = str(results)
+    #return results
     #json_data = json.loads(results)
-    #return results[0]['co']
+    return results[0]['co']
     #return json_data
     #return type(results)
    
